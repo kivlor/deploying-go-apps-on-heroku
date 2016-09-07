058f94a3c254831915b26bc776c77ec9249d3095
Andrew Gallagher <kivlor@gmail.com>
2016-09-07 16:26:47 +0700

    add a heroku app.json file

---
+++ app.js
@@ -0,0 +1,6 @@
+{
+  "name": "Hello World",
+  "description": "An example Go App",
+  "repository": "https://github.com/kivlor/the-caveats-of-deploying-go-apps-on-heroku",
+  "keywords": ["golang", "hello"]
+}
